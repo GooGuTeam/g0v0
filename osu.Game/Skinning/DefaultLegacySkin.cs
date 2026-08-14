11f39dd0c71540cc13ef05f8e6267d8d0df61633
MingxuanGame <MingxuanGame@outlook.com>
2026-08-14 15:42:52 +0800

    Update resources

--- osu.Game/Skinning/DefaultLegacySkin.cs
+++ osu.Game/Skinning/DefaultLegacySkin.cs
@@ -23,8 +23,8 @@ namespace osu.Game.Skinning
         public static SkinInfo CreateInfo() => new SkinInfo
         {
             ID = Skinning.SkinInfo.CLASSIC_SKIN, // this is temporary until database storage is decided upon.
-            Name = "osu! \"classic\" (2013)",
-            Creator = "team osu!",
+            Name = "osu! \"classic\" g0v0! ver (2026)",
+            Creator = "team osu! and GooGuTeam",
             Protected = true,
             InstantiationInfo = typeof(DefaultLegacySkin).GetInvariantInstantiationInfo()
         };
